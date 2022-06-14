8a0af1c9d5323adc9f7c872bf810df94e135f822
HafizdXD <101934492+HAFIZDXD@users.noreply.github.com>
2022-06-14 15:47:57 +0700

    Update V7.py

--- V7.py
+++ V7.py
@@ -221,18 +221,12 @@ def menu(my_name,my_id):
 	linex()
 	print('%s[%s01%s] %sCRACK PUBLIK'%(P,P,P,P));time.sleep(0.02)
 	print('%s[%s02%s] %sCRACK %sMASSAL'%(P,P,P,P,P));time.sleep(0.02)
-	print('%s[%s03%s] %s\033[32mJOIN GRUP WA%s'%(P,P,P,P,P));time.sleep(0.02)
-	print('%s[%s04%s] %s\033[95mCHAT ADMIN%s'%(P,P,P,P,P));time.sleep(0.02)
 	print('%s[%s06%s] %sEXIT%s'%(P,P,P,M,N));time.sleep(1)
 	jh = input(P+'['+P+'●'+P+']  MENU  ')
 	if jh in ['1','01']:
 		dump_publik()
 	elif jh in ['2','02']:
 		multidump()
-	elif jh in ['3','03']:
-		os.system('xdg-open https://chat.whatsapp.com/DIfkUz7xqzlDOcnf3xowql')
-	elif jh in ['4','04']:
-		os.system('xdg-open https://wa.me/6285758862225')
 	elif jh in ['6','06']:
 		os.system("rm -f .cok.txt")
 		print(h+'  ['+h+'●'+h+']  HOLD●●●')
